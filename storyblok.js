628e7d5f87d84b24e52feae37e9326579d2da2ee
svensmt <sven-maurice.schmitt@hotmail.com>
2025-11-07 17:12:47 +0100

    Aktualisierte Person-Komponente, Storyblok-Integration

---
+++ storyblok.js
@@ -0,0 +1,9 @@
+// storyblok.js
+import StoryblokClient from 'storyblok-js-client'
+
+const Storyblok = new StoryblokClient({
+  accessToken: process.env.STORYBLOK_API_KEY,
+  cache: { clear: 'auto', type: 'memory' }
+})
+
+export default Storyblok
